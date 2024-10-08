6e011dcf448dc42d6911077db8becde919ff5c8e
Halgord Zibari <hzibari@c1r4p8.hive.fi>
2024-10-08 12:01:15 +0300

    fixed errors, working now

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,6 +7,8 @@ int main() {
     while (true) {
         std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
         std::getline(std::cin, command);
+        if (std::cin.eof())
+            break;
 
         if (command == "ADD") {
             Contact newContact;
@@ -14,22 +16,32 @@ int main() {
 
             std::cout << "Enter First Name: ";
             std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;;
             newContact.setFirstName(input);
 
             std::cout << "Enter Last Name: ";
             std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;
             newContact.setLastName(input);
 
             std::cout << "Enter Nickname: ";
             std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;
             newContact.setNickname(input);
 
             std::cout << "Enter Phone Number: ";
             std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;
             newContact.setPhoneNumber(input);
 
             std::cout << "Enter Darkest Secret: ";
             std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;
             newContact.setDarkestSecret(input);
 
             phonebook.addContact(newContact);
@@ -37,9 +49,11 @@ int main() {
         } else if (command == "SEARCH") {
             phonebook.displayContacts();
             std::cout << "Enter the index of the contact to display: ";
-            int index;
-            std::cin >> index;
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::string input;
+            std::getline(std::cin, input);
+            if (std::cin.eof())
+                break;
+            int index = std::stoi(input);
             phonebook.displayContactDetails(index);
 
         } else if (command == "EXIT") {
